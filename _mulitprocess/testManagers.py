d530272abfad63eb542c32f4ae2b99b03f52ac74
captainliu-c <liucx.sky@gmail.com>
2018-01-19 15:53:26 +0800

    分布

---
+++ _mulitprocess/testManagers.py
@@ -0,0 +1,36 @@
+#testManagers.py
+
+import random, time, queue
+from multiprocessing.managers import BaseManager
+
+#send task
+task_queue = queue.Queue()
+#receive task
+result_queue = queue.Queue()
+
+class QueueManager(BaseManager):
+    pass
+
+QueueManager.register('get_task_queue', callable=lambda: task_queue)
+QueueManager.register('get_result_queue', callable=lambda: result_queue)
+
+manager = QueueManager(address=('' , 5000), authkey=b'abc')
+
+manager.start()
+
+task = manager.get_tak_queue()
+result = manager.get_result_queue()
+
+for i in range(10):
+    n = random.randint(0, 10000)
+    print('Put task %d...' % n)
+    task.put(n)
+
+print('Try get result...')
+for i in range(10):
+    r = result.get(timeout=10)
+    print('Result: %s' % r)
+
+manager.shutdown()
+print('master exit')
+
