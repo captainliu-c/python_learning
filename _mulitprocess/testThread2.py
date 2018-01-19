8ec4aebc494a404d0268f75197252160a5856181
captainliu-c <liucx.sky@gmail.com>
2018-01-19 10:43:40 +0800

    add lock
    增加lock机制

--- _mulitprocess/testThread2.py
+++ _mulitprocess/testThread2.py
@@ -3,7 +3,10 @@
 import time, threading
 
 balance = 0
+lock = threading.Lock()
 
+
+'''
 def change_it(n):
     global balance
     balance = balance + n
@@ -12,7 +15,21 @@ def change_it(n):
 def run_thread(n):
     for i in range(100000):
         change_it(n)
+'''
 
+
+def change_it(n):
+    global balance
+    balance = balance + n
+    balance = balance - n
+
+def run_thread(n):
+    for i in range(100000):
+        lock.acquire() #为什么需要try，acqure为什么放在try外面？
+        try:
+            change_it(n)
+        finally:
+            lock.release()
 
 t1 = threading.Thread(target=run_thread, args=(5,))
 t2 = threading.Thread(target=run_thread, args=(8,))
@@ -21,3 +38,4 @@ t2.start()
 t1.join()
 t2.join()
 print(balance)
+
